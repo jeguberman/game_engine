18d61dbc61b993e02e9035ba8aabce2514c1c245
John Guberman <jeguberman@gmail.com>
2018-02-20 20:15:54 -0500

    accidentally create animations

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-this gon' be my thing, you can just shuffle off
+game_engine is a personal project with the goal of creating a means to build simple platformers in HTML canvas with vanilla javascript and jquery and minimal libraries. The objective is to have something component based, but flexible.
+
+Phase 1: Create an engine capable of supporting a game with
+  1: Animations
+  2: Physics
+  2.5: Inputs
+  3: Collision Detection
+  4: Menus
+  5: An incremental Score
+  6: A game over condition
+
+  Basically to recreate my javascript project from my time at App Academy, but with cleaner, clearer code. game.johnguberman.com
+
+Eventually it would be nice to have scrolling and save states and angular collision, but eventually it's like, just use a game engine like phaser, amirite?
--- entry.js
+++ entry.js
@@ -1,60 +1,20 @@
-import Game from './lib/Game/game';
+// import Game from './lib/Game/game';
+import mockGame from './lib/mocks/mock_game';
 import merge from 'lodash/merge';
 
 
 const trueGame = ()=>{
   const view = document.getElementById('view');
-  let ctx = view.getContext('2d');
-  const game = new Game({ctx});
-  // debugger
+  const game = new Game();
+  game.setContext(view.getContext('2d'));
   view.width = game.view_width;
   view.height = game.view_height;
   game.start();
 
-
 };
 
 const falseGame = () => {
-
-
-  const BThing = () => {
-    return {
-      BVAL: 2,
-      func: function(){
-        console.log("from B");
-      },
-
-      Bfunc: function(){
-        console.log(this.BVAL);
-      },
-
-      otherFunc: function(){
-        this.Afunc();
-      }
-    };
-  };
-
-  const AThing = () => {
-    return {
-      AVAL: "A",
-      func: function(){
-        console.log("from A");
-      },
-
-      Afunc: function(){
-        console.log(this.AVAL);
-      }
-    };
-  };
-
-  let a = new AThing();
-  let b = new BThing();
-  let ab = merge({},a,b);
-  // ab.__proto__ = merge({},a.__proto__,b.__proto__);
-  let ap = a.__proto__;
-  let bp = b.__proto__;
-  let abp = Object.create({},ap,bp);
-  // ab.func();
+  mockGame();
 };
 
 
@@ -66,4 +26,4 @@ const switcher = (n) => {
   }
 };
 
-document.addEventListener('DOMContentLoaded', switcher(1));
+document.addEventListener('DOMContentLoaded', switcher(0));
